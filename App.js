63a89dc73661d5432c23f9d81e5b8f2a1090d74a
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-02-17 13:29:06 +0530

    Episode 01 - Inception Ended

--- App.js
+++ App.js
@@ -4,15 +4,23 @@
 *       <h1>I am h1 Tag</h1>
 *       <h2>I am h2 Tag</h2>
 *   </div>
+*   <div id="child2">
+*       <h1>I am h1 Tag</h1>
+*       <h2>I am h2 Tag</h2>
+*   </div>
 * </div>
 */
 
 const parent = React.createElement(
     "div", {id: "parent"}, 
-    React.createElement("div", {id: "child"}, [
+    [React.createElement("div", {id: "child"}, [
         React.createElement("h1", {}, "I am h1 Tag."),
         React.createElement("h2", {}, "I am h2 Tag."),
-    ])
+    ]),
+    React.createElement("div", {id: "child2"}, [
+        React.createElement("h1", {}, "I am h1 Tag."),
+        React.createElement("h2", {}, "I am h2 Tag."),
+    ])]
 );
 
 console.log(parent);
