6562f7322a303dbeed702dd156681ce09aaaa49f
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-02-17 09:22:12 +0530

    Inserting Sibling HTML Tags using Arrays of Children

--- App.js
+++ App.js
@@ -2,18 +2,17 @@
 * <div id="parent">
 *   <div id="child">
 *       <h1>I am h1 Tag</h1>
+*       <h2>I am h2 Tag</h2>
 *   </div>
 * </div>
 */
 
 const parent = React.createElement(
-    "div", 
-    {id: "parent"}, 
-    React.createElement(
-        "div", 
-        {id: "child"}, 
-        React.createElement("h1", {}, "I am h1 Tag.")
-    )
+    "div", {id: "parent"}, 
+    React.createElement("div", {id: "child"}, [
+        React.createElement("h1", {}, "I am h1 Tag."),
+        React.createElement("h2", {}, "I am h2 Tag."),
+    ])
 );
 
 console.log(parent);
