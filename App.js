8a712c290eea4eb7a7174275c3754cfdf94f8427
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-02-17 08:42:40 +0530

    Created and Inserted Nested HTML Structure in React

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
-const heading = React.createElement(
-    "h1", {
-        id: "heading",
-        xyz: "abc",}, 
-    "Hello World from React !");
+/**
+* <div id="parent">
+*   <div id="child">
+*       <h1>I am h1 Tag</h1>
+*   </div>
+* </div>
+*/
+
+const parent = React.createElement(
+    "div", 
+    {id: "parent"}, 
+    React.createElement(
+        "div", 
+        {id: "child"}, 
+        React.createElement("h1", {}, "I am h1 Tag.")
+    )
+);
+
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
