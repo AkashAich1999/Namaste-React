077a5d3a63c2861018103d08c0eb4d12261532e8
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-02-18 16:47:08 +0530

    Episode 02 - Igniting our App Finally Completed

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-/**
-* <div id="parent">
-*   <div id="child">
-*       <h1>I am h1 Tag</h1>
-*       <h2>I am h2 Tag</h2>
-*   </div>
-*   <div id="child2">
-*       <h1>I am h1 Tag</h1>
-*       <h2>I am h2 Tag</h2>
-*   </div>
-* </div>
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
     "div", {id: "parent"}, 
     [React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "I am h1 Tag."),
-        React.createElement("h2", {}, "I am h2 Tag."),
+        React.createElement("h1", {}, "This is Namaste React. 🚀"),
+        React.createElement("h2", {}, "Akash Aich is Learning!"),
     ]),
     React.createElement("div", {id: "child2"}, [
         React.createElement("h1", {}, "I am h1 Tag."),
