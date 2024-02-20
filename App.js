5af8fa887cfbb6184db4842757403e4ccb8e2727
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-02-20 14:21:34 +0530

    Episode 03 - Laying the Foundation (Including React Element inside React Component & Vice Versa)

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const elem = <span>(React Element 2) | </span>
+
+// React Component
 const Title = () => (
     <h1 className="head" tabIndex="5">
-        Namaste React using JSX ! 🚀
+        Namaste React using JSX (React Component) ! 🚀
     </h1>
 );
+
+// React Element 
+const title = (
+    <h1 className="head" tabIndex="5">
+        {elem}
+        Namaste React using JSX (React Element 1) ! 🚀
+    </h1>
+);
+
+const number = 770;
 
 // Component Composition
 const HeadingComponent = () => (
     <div className="container">
+        {title}
+        <h1>{number + 7}</h1>
         <Title />
         <h1 className="heading">Namaste React Functional Component !</h1>
     </div>
