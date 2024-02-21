87bcb132ce78c6d7a4495c8104f354b2730bcb56
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2024-02-21 17:26:31 +0530

    Episode 04 - Talk is cheap, show me the code! (Completed)

--- App.js
+++ App.js
@@ -58,6 +58,7 @@ const RestaurantCard = (props) => {
     );
 }
 
+
 const resList = [
     {
     type: "restaurant",
@@ -1948,21 +1949,11 @@ const Body = () => {
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestaurantCard resData={resList[0]} />
-                <RestaurantCard resData={resList[1]} />
-                <RestaurantCard resData={resList[2]} />
-                <RestaurantCard resData={resList[3]} />
-                <RestaurantCard resData={resList[4]} />
-                <RestaurantCard resData={resList[5]} />
-                <RestaurantCard resData={resList[6]} />
-                <RestaurantCard resData={resList[7]} />
-                <RestaurantCard resData={resList[8]} />
-                <RestaurantCard resData={resList[9]} />
-                <RestaurantCard resData={resList[10]} />
-                <RestaurantCard resData={resList[11]} />
-                <RestaurantCard resData={resList[12]} />
-                <RestaurantCard resData={resList[13]} />
-                <RestaurantCard resData={resList[14]} />
+                {
+                    resList.map((restaurant) => (
+                        <RestaurantCard  key={restaurant.data.id} resData={restaurant} />
+                    ))
+                }
             </div>
         </div>
     );
